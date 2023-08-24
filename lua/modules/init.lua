21dd71eec24cfa34da81f9ec6af815eadf9f8fec
Shadito <shadito@live.com.mx>
2023-08-24 00:45:49 -0600

    Update packer

--- lua/modules/init.lua
+++ lua/modules/init.lua
@@ -1,153 +1,195 @@
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+      'git',
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/wbthomason/packer.nvim',
+      install_path
+    })
+    vim.cmd [[packadd packer.vim]]
+    return true
+  end
+  return false
 end
 
-local packer = require("packer")
+local packer_bootstrap = ensure_packer()
 
-return packer.startup(function(use)
-  use("wbthomason/packer.nvim")
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
+  -- Routes of configs files
   local function conf(name)
-    return require(string.format("modules.config.%s", name))
+    return require(string.format('modules.config.%s', name))
   end
 
-  use({ -- Themes
-    "marko-cerovac/material.nvim",
-    "EdenEast/nightfox.nvim",
-    "rebelot/kanagawa.nvim",
-    "folke/tokyonight.nvim",
-    config = conf("colors"),
-  })
-  use({ -- Treesiter
-    "nvim-treesitter/nvim-treesitter",
+  -- My plugins here
+  -- Themes
+  use {
+    {'marko-cerovac/material.nvim'},
+    {'EdenEast/nightfox.nvim'},
+    {'rebelot/kanagawa.nvim'},
+    {'folke/tokyonight.nvim'},
+    config = conf('colors')
+  }
+
+  -- Treesiter
+  use {
+    'nvim-treesitter/nvim-treesitter',
     run = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
+      require('nvim-treesitter.install').update({ with_sync = true })
     end,
-    config = conf("nvim-treesitter"),
+    config = conf('nvim-treesitter'),
     requires = {
-      "nvim-treesitter/playground",
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "nvim-treesitter/nvim-treesitter-refactor",
-      "p00f/nvim-ts-rainbow",
-      "windwp/nvim-ts-autotag",
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-  })
-  use({ -- Finder
-    "nvim-telescope/telescope.nvim",
-    tag = '0.1.2',
-    config = conf("telescope"),
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-file-browser.nvim",
-    },
-  })
-  use({ -- File explorer
-    'nvim-tree/nvim-tree.lua',
-    config = conf("nvim-tree"),
-  })
-  use({ -- Icons
-     'nvim-tree/nvim-web-devicons',
-    config = conf("nvim-web-devicons"),
-  })
-  use({ -- colorizer
-    "norcalli/nvim-colorizer.lua",
-    config = conf("colorizer"),
-  })
-  use({ -- Statusline
-    "nvim-lualine/lualine.nvim",
-    config = conf("lualine"),
-  })
-  use({ -- Bufferline Tabs
-    "akinsho/bufferline.nvim",
-    tag = "v2.*",
-    config = conf("bufferline"),
-  })
-  use({ -- Indent Blankline
-    "lukas-reineke/indent-blankline.nvim",
-    config = conf("indent-blankline"),
-  })
-  use({ -- Notifications
-    "rcarriga/nvim-notify",
-  })
-  use({ -- Lsp Stuff
-    "williamboman/mason.nvim",
-    config = conf("mason"),
-    requires = {
-      "williamboman/mason-lspconfig.nvim",
+      {'nvim-treesitter/playground'},
+      {'nvim-treesitter/nvim-treesitter-textobjects'},
+      {'nvim-treesitter/nvim-treesitter-refactor'},
+      {'p00f/nvim-ts-rainbow'},
+      {'windwp/nvim-ts-autotag'},
+      {'JoosepAlviste/nvim-ts-context-commentstring'}
     }
-  })
-  use({ -- Lsp
-    "neovim/nvim-lspconfig",
-    config = conf("lsp"),
+  }
+
+  -- Finder
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
+    config = conf('telescope'),
     requires = {
-      "b0o/schemastore.nvim", -- JSON schema for jsonls
-      "ray-x/lsp_signature.nvim",
-      "jose-elias-alvarez/nvim-lsp-ts-utils",
-      "RRethy/vim-illuminate",
-      "tami5/lspsaga.nvim",
-      "folke/lsp-colors.nvim",
-    },
-  })
-  use({
-    "jose-elias-alvarez/null-ls.nvim", -- Format
-    config = conf("lsp/null-ls"),
-  })
-  use({ -- Prettier
-    "MunifTanjim/prettier.nvim",
-    config = conf("prettier"),
-  })
-  use({ -- Trouble
-    "folke/trouble.nvim",
-  })
-  use({ -- Autocompletion plugin
-    "hrsh7th/nvim-cmp",
-    config = conf("nvim-cmp"),
+      {'nvim-lua/plenary.nvim'},
+      {'nvim-telescope/telescope-file-browser.nvim'}
+    }
+  }
+
+  -- File explorer
+  use {
+    'nvim-tree/nvim-tree.lua',
+    config = conf('nvim-tree')
+  }
+
+  -- Icons
+  use {
+     'nvim-tree/nvim-web-devicons',
+    config = conf('nvim-web-devicons')
+  }
+
+  -- colorizer
+  use {
+    'norcalli/nvim-colorizer.lua',
+    config = conf('colorizer')
+  }
+
+  -- Statusline
+  use {
+    'nvim-lualine/lualine.nvim',
+    config = conf('lualine')
+  }
+
+  -- Bufferline Tabs
+  use {
+    'akinsho/bufferline.nvim',
+    tag = 'v2.*',
+    config = conf('bufferline')
+  }
+
+  --Indent Blankline
+  use {
+    'lukas-reineke/indent-blankline.nvim',
+    config = conf('indent-blankline')
+  }
+
+  -- Notifications
+  use 'rcarriga/nvim-notify'
+
+  -- Lsp Stuff
+  use {
+    'williamboman/mason.nvim',
+    config = conf('mason'),
+    requires = {'williamboman/mason-lspconfig.nvim'}
+  }
+
+  -- Lsp
+  use {
+    'neovim/nvim-lspconfig',
+    config = conf('lsp'),
     requires = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-emoji",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-path",
-      "onsails/lspkind-nvim", -- Enables icons on completions
+      {'b0o/schemastore.nvim'},
+      {'ray-x/lsp_signature.nvim'},
+      {'jose-elias-alvarez/nvim-lsp-ts-utils'},
+      {'RRethy/vim-illuminate'},
+      {'tami5/lspsaga.nvim'},
+      {'folke/lsp-colors.nvim'}
+    }
+  }
+
+  -- Format
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    config = conf('lsp/null-ls')
+  }
+
+  -- Prettier
+  use {
+    'MunifTanjim/prettier.nvim',
+    config = conf('prettier')
+  }
+
+  --Trouble
+  use 'folke/trouble.nvim'
+  
+  -- Autocompletion plugin
+  use {
+    'hrsh7th/nvim-cmp',
+    config = conf('nvim-cmp'),
+    requires = {
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-emoji'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+      {'hrsh7th/cmp-path'},
+      {'onsails/lspkind-nvim'},
       { -- Snippets
-        "L3MON4D3/LuaSnip",
+        'L3MON4D3/LuaSnip',
         requires = {
-          "saadparwaiz1/cmp_luasnip",
-          "rafamadriz/friendly-snippets",
-        },
-      },
-    },
-  })
-  use({ -- Autopais
-    "windwp/nvim-autopairs",
-    config = conf("nvim-autopairs"),
-  })
-  use({ -- GitSigns
-    "lewis6991/gitsigns.nvim",
-    config = conf("gitsigns"),
-  })
-  use({ -- Git
-    "dinhhuy258/git.nvim",
-    config = conf("nvim-git"),
-  })
-  use({ -- Rest
-    "rest-nvim/rest.nvim",
-    config = conf("nvim-rest"),
-    requires = {
-      "nvim-lua/plenary.nvim"
-    },
-  })
+          {'saadparwaiz1/cmp_luasnip'},
+          {'rafamadriz/friendly-snippets'}
+        }
+      }
+    }
+  }
+
+  --Autopairs
+  use {
+    'windwp/nvim-autopairs',
+    config = conf('nvim-autopairs')
+  }
+
+  -- GitSigns
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = conf('gitsigns')
+  }
+
+  -- Git
+  use {
+    'dinhhuy258/git.nvim',
+    config = conf('nvim-git'),
+  }
+
+  -- Rest
+  use {
+    'rest-nvim/rest.nvim',
+    config = conf('nvim-rest'),
+    requires = {'nvim-lua/plenary.nvim'}
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
   if packer_bootstrap then
-    packer.sync()
+    require('packer').sync()
   end
 end)
